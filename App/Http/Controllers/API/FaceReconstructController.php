4b3c0af1cddf8c8b2031995a42a08826cb4e0d34
aMoQuder <anaahmedmohamed86@gmail.com>
2026-06-06 00:47:49 +0300

    add model  face reconstruction in controller only with out service

--- App/Http/Controllers/API/FaceReconstructController.php
+++ App/Http/Controllers/API/FaceReconstructController.php
@@ -13,7 +13,6 @@ use Illuminate\Support\Facades\Log;
 class FaceReconstructController extends Controller {
 
     public function store( Request $request ) {
-        set_time_limit(0);
         $validated = $request->validate( [
             'image' => 'required|image|max:4096',
         ] );
@@ -21,8 +20,8 @@ class FaceReconstructController extends Controller {
         if ( $request->hasFile( 'image' ) ) {
             $image = $request->image;
             $imageName = rand( 1, 1000 ) . time() . '.' . $image->extension();
-            $image->move( public_path( 'FaceReconstruct/img/' ), $imageName );
-            $imageName = 'FaceReconstruct/img/'.$imageName;
+            $image->move( public_path( 'FaceRecog/img/' ), $imageName );
+            $imageName = 'FaceRecog/img/'.$imageName;
         }
         try {
 
@@ -32,63 +31,42 @@ class FaceReconstructController extends Controller {
                 file_get_contents( public_path( $imageName ) ),
                 $imageName
             )
-            ->post( 'https://anastamer-deepface-project.hf.space/forensicAnalysis' );
+            ->post( 'https://anastamer-deepface-communicated.hf.space/recognize' );
+            $data = $response->json();
+            if ( !$response->successful() ) {
 
-            $aiResponse= $response->json();
-            if ( !$aiResponse || !isset( $aiResponse[ 'enhanced_image_base64' ] ) ) {
+                if ( isset( $data[ 'detail' ] ) ) {
+                    return response()->json( [
+                        'message' => $data[ 'detail' ], // No face detected in the image. Please try again with a different image.
+                    ], 400 );
+                }
                 return response()->json( [
-                    'status'  => 'error',
-                    'message' => 'AI reconstruction failed or model does not found any person in image.'
+                    'message' => 'AI service error',
+                    'error' => $data
                 ], 500 );
             }
-
-            $face = $aiResponse[ 'faces' ][ 0 ] ?? null;
-
-            $age    = $face[ 'age_info' ][ 'age_range' ] ?? 'Unknown';
-            $gender = $face[ 'gender_info' ][ 'gender' ] ?? 'Unknown';
-
-            try {
-                $base64String = $aiResponse[ 'enhanced_image_base64' ];
-
-                if ( preg_match( '/^data:image\/(\w+);base64,/', $base64String, $type ) ) {
-                    $base64String = substr( $base64String, strpos( $base64String, ',' ) + 1 );
-                }
-
-                $imageData = base64_decode( $base64String );
-
-                if ( !$imageData ) {
-                    throw new \Exception( 'Invalid Base64 format.' );
-                }
-
-                $fileName = 'reconstructed_faces/' . Str::uuid() . '.jpg';
-                Storage::disk( 'public' )->put( $fileName, $imageData );
-
-            } catch ( \Exception $e ) {
-                Log::error( 'Image Save Error:', [ 'msg' => $e->getMessage() ] );
-                return response()->json( [ 'status' => 'error', 'message' => 'Failed to save processed image.' ], 500 );
-            }
             $model = model_ai::create( [
-                'models'=>'face reconstruction'
+                'models'=>'face recognation'
             ] );
+
             return response()->json( [
                 'status'  => 'success',
-                'message' => 'Analysis successful.',
+                'message' => 'Analysis face recognation completed successfully.',
                 'model_used'=>$model->models,
-
                 'data'    => [
                     'phenotypes'  => [
-                        'age'       => $age,
-                        'gender'    => $gender,
-                        'image_url' => asset( 'storage/' . $fileName ),
-                    ]
-                ]
-            ] );
+                        'name' => $data[ 'identities' ][ 0 ],
+                        'image' => $imageName
+                        ? asset( $imageName )
+                        : null, ] ]
+                    ] );
 
-        } catch ( \Exception $e ) {
-            return response()->json( [
-                'message' => 'Error: ' . $e->getMessage()
-            ], 500 );
+                } catch ( \Exception $e ) {
+                    return response()->json( [
+                        'message' => 'Error: ' . $e->getMessage()
+                    ], 500 );
+                }
+
         }
-
-    }
 }
+
